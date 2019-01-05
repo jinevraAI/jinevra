cfe471229cfbe1b5f793436891bd197f0b2ffbe7
eleanorstrib <hello@eleanorstrib.com>
2019-01-05 14:50:58 -0800

    added first files, folders to the package index

---
+++ jinevra/__init__.py
@@ -0,0 +1,5 @@
+def about():
+    return("jinevra democratizes AI-based language analysis. Learn more at jinevra.ai")
+
+def confirm_import():
+    return("You've imported the Jinevra library!")
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='jinevra',
+    version='0.1',
+    description='jinevra democratizes AI-based language analysis',
+    url='http://jinevra.ai'
+    author='eleanorstrib',
+    author_email='hello@jinevra.ai',
+    license='MIT',
+    packages=['jinevra'],
+    zip_safe=False
+)
