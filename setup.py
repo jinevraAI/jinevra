683cf9d35f132bdaac2e17b80a52d308a8ad40db
eleanorstrib <hello@eleanorstrib.com>
2019-01-09 19:48:55 -0800

    updated settings file to add domains

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from setuptools import setup
 
-setup(
-    name='jinevra',
-    version='0.1',
-    description='jinevra democratizes AI-based language analysis',
-    url='http://jinevra.ai'
-    author='eleanorstrib',
-    author_email='hello@jinevra.ai',
-    license='MIT',
-    packages=['jinevra'],
-    zip_safe=False
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jinevra",
+    version="0.0.1",
+    author="Eleanor Stribling",
+    author_email="hello@jinevra.ai",
+    description="jinevra democratizes AI for natural language",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://jinevra.herokuapp.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
